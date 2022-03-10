9bbef43598ebe9bb3e01e390f60cc4ddc4c856c0
Abhijit <Abhijit.patel@tothenew.com>
2022-03-11 00:16:21 +0530

    Jpa exercise day1

---
+++ JpaDay1/src/main/java/com/SpringData/SpringDataAssignment1/SpringDataAssignment1Application.java
@@ -0,0 +1,12 @@
+package com.SpringData.SpringDataAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAssignment1Application.class, args);
+	}
+}
